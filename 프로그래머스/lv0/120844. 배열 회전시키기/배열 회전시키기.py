1552e0c6ce7283e6e8fd1adea8a16568d18ecfb6
JounKK <125840482+JounKK@users.noreply.github.com>
2023-04-07 17:36:18 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+def solution(numbers, direction):
+    numbers_deque = deque(numbers)
+    if direction == 'right': 
+        numbers_deque.rotate(1)
+    else: 
+        numbers_deque.rotate(-1)
+        print(numbers_deque)
+    return list(numbers_deque)
