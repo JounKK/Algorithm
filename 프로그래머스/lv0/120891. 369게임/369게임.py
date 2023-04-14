bbbd5548ec1e043b24fb5151ce0b47cde6f8a007
JounKim <125840482+JounKK@users.noreply.github.com>
2023-04-14 11:11:11 +0900

    [level 0] Title: 369게임, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.py
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -1,6 +1,12 @@
+# def solution(order):
+#     count = 0
+#     count += str(order).count('3')
+#     count += str(order).count('6')
+#     count += str(order).count('9')
+#     return count
+
+# 정규표현식
+import re
+
 def solution(order):
-    count = 0
-    count += str(order).count('3')
-    count += str(order).count('6')
-    count += str(order).count('9')
-    return count
+    return len(re.findall('[369]', str(order)))
