6d43e08c8232a5893920c9cd7cc01a5cbbc6300a
JounKim <125840482+JounKK@users.noreply.github.com>
2023-04-14 09:25:47 +0900

    [level 0] Title: 모음 제거, Time: 0.09 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
@@ -1,2 +1,13 @@
-def solution(my_string):  
-    return ''.join(list(filter(lambda x:x not in 'aeiou', my_string)))
+# 필터
+# def solution(my_string):  
+#     return ''.join(list(filter(lambda x:x not in 'aeiou', my_string)))
+
+# replace
+# def solution(my_string):
+#     return my_string.replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', '')
+
+# 정규표현식
+import re 
+
+def solution(my_string): 
+    return re.sub(r'[aeiou]','',my_string)
