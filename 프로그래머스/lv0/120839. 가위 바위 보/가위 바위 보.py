660d9bdeb24caf547cf067f709bf7c673accd60d
JounKim <125840482+JounKK@users.noreply.github.com>
2023-04-15 23:01:36 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,18 @@
+# def solution(rsp):
+#     answer = ''
+#     for i in rsp:
+#         if i == '2':
+#             answer += '0'
+#         elif i == '0':
+#             answer += '5'
+#         else:
+#             answer += '2'
+    
+#     return answer
+
+
+def solution(rsp):
+    d = {'0':'5', '2':'0', '5':'2'}
+    answer = ''
+    
+    return ''.join([d[i] for i in rsp])
