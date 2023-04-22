840e882f3c2d9ebde43b577127ffaa8918e89c20
JounKim <125840482+JounKK@users.noreply.github.com>
2023-04-22 23:28:39 +0900

    Update 외계어 사전.py

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.py
@@ -6,4 +6,11 @@ def solution(spell, dic):
     return 1 if len(new) >= 1 else 2
 
 
-
+# 강사님 답
+# sorted(스트링단어) > 한자씩 리스트 정렬된다. 
+def solution(spell, dic):
+    for d in dic:
+        if sorted(d) == sorted(spell):
+            return 1
+        
+    return 2
