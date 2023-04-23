8f7f7da49ca26d328c38f6712acb444967a9ed2b
JounKim <125840482+JounKK@users.noreply.github.com>
2023-04-23 19:09:31 +0900

    Update 문자열 밀기.py

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -8,3 +8,14 @@ def solution(A, B):
             return i
 
     return -1
+
+
+# 다른 답
+# 2개를 이어붙여서 찾아보면 됨
+# 회전형태로 돌아가는 것을 1차원 배열로 만들어서 인덱스를 찾는 방법
+# 2차원으로 풀어야하는 것을 1차원으로 축소시켜서 푸는 방법 !
+def solution1(A, B):
+    return (B*2).find(A)
+
+solution1("hellzo", "ohell")
+
