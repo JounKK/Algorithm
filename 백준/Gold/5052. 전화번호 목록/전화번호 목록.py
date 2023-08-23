56a602993dada75dd1560325eb5121c421f35752
JounKim <125840482+JounKK@users.noreply.github.com>
2023-08-23 23:00:09 +0900

    [Gold IV] Title: 전화번호 목록, Time: 168 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,50 +1,21 @@
 import sys
 input = sys.stdin.readline
-class Node:
-    def __init__(self, key, data=None):
-        self.key = key
-        self.data = data
-        self.children = {}
- 
-class Trie:
-    def __init__(self):
-        self.head = Node(None)
- 
-    def insert(self, string):
-        current_node = self.head
-        for char in string:
-            if char not in current_node.children:
-                current_node.children[char] = Node(char)
-            current_node = current_node.children[char]
- 
-        current_node.data = string
- 
-    def search(self, string):
-        current_node = self.head
-        for char in string:
-            current_node = current_node.children[char]
- 
-        if current_node.children:
-            return False
-        else:
-            return True    
- 
-for _ in range(int(input())):
+
+t = int(input())
+
+for _ in range(t):
     n = int(input())
-    trie = Trie()
-    phone = []
+    phonebook = []
+
     for _ in range(n):
-        w = input().rstrip()
-        phone.append(w)
-        trie.insert(w)
-    
-    res = True
-    phone.sort()
-    for p in phone:
-        if not trie.search(p):
-            res = False
+        phonebook.append(input().rstrip())
+
+    phonebook.sort()
+    flag = True
+
+    for i in range(n-1):
+        if phonebook[i] == phonebook[i+1][:len(phonebook[i])]:
+            flag = False
             break
-    if res:
-        print("YES")
-    else:
-        print("NO")
+
+    print("YES") if flag else print("NO")
