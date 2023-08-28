96f58e02f4c923c1c64c7459380030ca52c0450c
JounKim <125840482+JounKK@users.noreply.github.com>
2023-08-29 01:09:44 +0900

    [Bronze I] Title: 진법 변환 2, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -1,9 +1,23 @@
-system = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-N, B = map(int, input().split())
-ans = ''
+# BOJ 11005번
+n, b = map(int, input().split())
 
-while N != 0:
-    ans += str(system[N%B])
-    N //= B
+highest_degree = 0
+while b ** highest_degree <= n:
+    highest_degree += 1
+highest_degree -= 1 # 최고차수 구하고
 
-print(ans[::-1])
+quotient = []
+for i in range(highest_degree, -1, -1): # 차수돌아가면서 몫 넣어주고
+    quotient.append(n // (b ** i))
+    n %= (b ** i)
+# print(quotient)
+
+dic = dict()
+for i in range(10): # 0~10까지 n진법 매핑
+    dic[i] = str(i)
+for i in range(10, 36): # A~Z까지 매핑
+    dic[i] = chr(55 + i)
+# print(dic)
+
+for i in quotient:
+    print(dic[i], end='')
